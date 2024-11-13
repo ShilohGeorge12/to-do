c65c89a096c8b6082dd3c92b0f1192c500c1941e
Shiloh George <shinohgeorge2019@gmail.com>
2024-11-13 11:49:33 +0100

    debugging get todos route

--- dist/controllers/index.js
+++ dist/controllers/index.js
@@ -18,6 +18,7 @@ export const getTodos = async (req, res) => {
         TodoModel.countDocuments(), // Count total documents in parallel
     ]);
     const totalPages = Math.ceil(totalItems / limit);
+    console.log(todos);
     res.status(200).json({
         todos,
         totalItems,
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -24,6 +24,7 @@ export const getTodos = async (req: Request, res: Response) => {
 	]);
 
 	const totalPages = Math.ceil(totalItems / limit);
+	console.log(todos);
 
 	res.status(200).json({
 		todos,
